56ca93ce961ecbd6da52aae463d372b0b1273d11
jeongyeonpark <120764374+jeongyeonpark@users.noreply.github.com>
2024-07-04 19:44:31 +0900

    Time: 63 ms (37.31%), Space: 17 MB (13.83%) - LeetHub

--- LeetCode/0383-ransom-note/0383-ransom-note.py
+++ LeetCode/0383-ransom-note/0383-ransom-note.py
@@ -1,7 +1,15 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        for rn in ransomNote:
+        """for rn in ransomNote:
             if ransomNote.count(rn) > magazine.count(rn):
                 return False
                 break
+        return True"""
+
+        mag_list = [x for x in magazine]
+        for rn in ransomNote:
+            if rn in mag_list:
+                mag_list.remove(rn)
+            else:
+                return False
         return True
