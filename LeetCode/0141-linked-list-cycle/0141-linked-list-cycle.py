b89763040656e42ce1dfd8637e051b2fd8ff7743
jeongyeonpark <120764374+jeongyeonpark@users.noreply.github.com>
2024-07-27 22:13:08 +0900

    Time: 768 ms (5.04%), Space: 19 MB (50.67%) - LeetHub

--- LeetCode/0141-linked-list-cycle/0141-linked-list-cycle.py
+++ LeetCode/0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,21 +6,34 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # if not head:
+        #     return False
+
+        # node = head
+        # next_node = head.next
+        # visited_node = []
+        # while not next_node:
+        #     node_id = id(node)
+        #     if not node_id in visited_node:
+        #         visited_node.append(node_id)
+        #         node = next_node
+        #         next_node = next_node.next
+
+        #     else:
+        #         return True
+        # return False
         if not head:
             return False
 
         node = head
-        next_node = head.next
-        visited_node = []
-        while node != None:
-            node_id = id(node)
-            if not next_node:
-                return False
 
-            elif not node_id in visited_node:
-                visited_node.append(node_id)
-                node = next_node
-                next_node = next_node.next
-
-            else:
+        visited_node =[]
+        
+        while node.next:
+            id_node = id(node)
+            if id_node in visited_node:
                 return True
+            visited_node.append(id_node)
+            node = node.next
+
+        return False
