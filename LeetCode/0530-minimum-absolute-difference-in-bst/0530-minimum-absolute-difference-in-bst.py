6faa84f35a3354853e07472d9b28d8bd3601338c
jeongyeonpark <120764374+jeongyeonpark@users.noreply.github.com>
2024-07-30 20:48:32 +0900

    Time: 48 ms (54.76%), Space: 18.4 MB (11.29%) - LeetHub

--- LeetCode/0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.py
+++ LeetCode/0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.py
@@ -6,38 +6,58 @@
 #         self.right = right
 class Solution:
     def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
-        v_list = []
-        q = deque([root])
+        q = []
+        def add_node(node):
+            if not node:
+                return
+            add_node(node.left)
+            q.append(node.val)
+            add_node(node.right)
+            #q.append(node.right)
+
+        add_node(root)
         min_v = 10**5
 
-        while q:
-            curr_node = q.popleft()
-            if curr_node.val in v_list:
-                return 0
-            else:
-                if len(v_list) > 2:
-                    v = curr_node.val
-                    v_list = sorted(v_list)
+        for i in range(len(q)-1):
+            min_v = min(min_v, abs(q[i]-q[i+1]))
+            if min_v == 1:
+                return 1
 
-                    if v < v_list[0]:
-                        min_v = min(abs(v - v_list[0]), min_v)
-                    elif v > v_list[-1]:
-                        min_v = min(abs(v - v_list[-1]), min_v)
-                    else:
-                        for i in range(len(v_list)-1):
-                            if v>v_list[i] and v<v_list[i+1]:
-                                min_v_ = min(abs(v - v_list[i]), abs(v - v_list[i+1]))
-                                min_v = min(min_v_, min_v)
-
-                else:
-                    for v in v_list:
-                        min_v = min(abs(curr_node.val - v), min_v)
-
-                v_list.append(curr_node.val)
-            
-            if curr_node.left:
-                q.append(curr_node.left)
-
-            if curr_node.right:
-                q.append(curr_node.right)
         return min_v
+
+
+        # v_list = []
+        # q = deque([root])
+        # min_v = 10**5
+
+        # while q:
+        #     curr_node = q.popleft()
+        #     if curr_node.val in v_list:
+        #         return 0
+        #     else:
+        #         if len(v_list) > 2:
+        #             v = curr_node.val
+        #             v_list = sorted(v_list)
+
+        #             if v < v_list[0]:
+        #                 min_v = min(abs(v - v_list[0]), min_v)
+        #             elif v > v_list[-1]:
+        #                 min_v = min(abs(v - v_list[-1]), min_v)
+        #             else:
+        #                 for i in range(len(v_list)-1):
+        #                     if v>v_list[i] and v<v_list[i+1]:
+        #                         min_v_ = min(abs(v - v_list[i]), abs(v - v_list[i+1]))
+        #                         min_v = min(min_v_, min_v)
+
+        #         else:
+        #             for v in v_list:
+        #                 min_v = min(abs(curr_node.val - v), min_v)
+
+        #         v_list.append(curr_node.val)
+            
+        #     if curr_node.left:
+        #         q.append(curr_node.left)
+
+        #     if curr_node.right:
+        #         q.append(curr_node.right)
+        # return min_v
