0211dc4b3ae3b8e4824608579aa54a5deceb533b
jeongyeonpark <120764374+jeongyeonpark@users.noreply.github.com>
2024-08-01 00:46:51 +0900

    Time: 54 ms (6.36%), Space: 16.6 MB (58.29%) - LeetHub

--- LeetCode/0071-simplify-path/0071-simplify-path.py
+++ LeetCode/0071-simplify-path/0071-simplify-path.py
@@ -8,50 +8,38 @@ class Solution:
         d = 0
         new_path = '/'
         for i in range(1,len(path)):
-            if path[i] == '/' or i == len(path)-1:
-                if path[i] != '/':
-                    if n == 0:#
-                        new_path+='/'#
+            #print(f'before: {new_path} , n:{n}, w:{w}, d:{d}')
+            if path[i] != '/' or i == len(path)-1:
+                if n == 0:
+                    new_path+='/'
+                    n=1
+                if path[i] == '.':
+                    d+=1
                     new_path += path[i]
-                    
-                    if path[i] == '.':
-                        d+=1
-                    else:
-                        w +=1
+                elif path[i] != '/':
+                    w +=1
+                    new_path += path[i]
 
+            if path[i] == '/' or i == len(path)-1:
                 if w == 0 and d == 0:
                     pass
-                elif d == 0:
+                elif w != 0 and d == 0:
                     w = 0
                     n = 0
-                elif w == 0:
+                elif w == 0 and d != 0:
+                    n = 0
                     if d == 1:
                         new_path = new_path[:-2]
-                        n=0
                     elif d == 2:
                         if len(new_path.split('/')) < 4:
                             new_path = '/'
                             n = 1
                         else:
                             new_path = '/'.join(new_path.split('/')[:-2])
-                            n=0
-                    else:
-                        n = 0
                     d = 0
                 else:
                     w, d, n = 0, 0, 0
-
-            else:
-                if n==0:
-                    new_path+='/'
-                    n=1
-                 
-                if path[i] == '.':
-                    d +=1
-                    new_path+='.'
-                else:
-                    w +=1
-                    new_path+=path[i]
+            #print(f'after: {new_path} , n:{n}, w:{w}, d:{d}')
 
         if len(new_path)==0:
             return '/'
