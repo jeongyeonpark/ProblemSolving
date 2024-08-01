76dd77c0d7394a3fdd0d6b0f815edd3b78a889bc
jeongyeonpark <120764374+jeongyeonpark@users.noreply.github.com>
2024-08-01 21:21:19 +0900

    Time: 50 ms (6.42%), Space: 16.5 MB (29.32%) - LeetHub

--- LeetCode/0086-partition-list/0086-partition-list.py
+++ LeetCode/0086-partition-list/0086-partition-list.py
@@ -7,15 +7,19 @@ class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
         prev_list = []
         next_list = []
+        list_ = []
 
         curr_node = head
         while curr_node:
+            list_.append(curr_node.val)
             if curr_node.val < x:
                 prev_list.append(curr_node.val)
             else:
                 next_list.append(curr_node.val)
             curr_node = curr_node.next
 
+        if list_ == prev_list+next_list:
+            return head
         curr_node = head
         for n in prev_list+next_list:
             curr_node.val = n
