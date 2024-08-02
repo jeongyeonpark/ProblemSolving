e99aba8c7c92dda03535eaccfd8ff0c7382ea211
jeongyeonpark <120764374+jeongyeonpark@users.noreply.github.com>
2024-08-02 23:42:22 +0900

    Time: 944 ms (34.27%), Space: 22.1 MB (29.6%) - LeetHub

--- LeetCode/0134-gas-station/0134-gas-station.py
+++ LeetCode/0134-gas-station/0134-gas-station.py
@@ -1,31 +1,23 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        curr_sum = 0
+        if sum(gas) < sum(cost):
+            return -1
+
         total_sum = 0 
         start_idx = -1
         i = 0
         while i < len(gas):
-            #print(f'stat: {i}')
-            curr_sum = 0
-            if gas[i]-cost[i] >= 0:
-                curr_sum += gas[i]-cost[i]
-                start_idx = i
-                i+=1
+            curr_sum = gas[i]-cost[i]
+            i+=1
+            if curr_sum >= 0:
+                start_idx=i-1
                 while i < len(gas) and curr_sum+gas[i]-cost[i] >= 0 :
-                    #print(f'pos while : {i}')
                     curr_sum += gas[i]-cost[i]
-                    #print(f'pos curr_sum : {curr_sum}')
                     i+=1
-                total_sum+=curr_sum
             else:
-                curr_sum += gas[i]-cost[i]
-                i+=1
                 while i < len(gas) and gas[i]-cost[i] < 0 :
-                    #print(f'neg while : {i}')
                     curr_sum += gas[i]-cost[i]
-                    #print(f'neg sum : {curr_sum}')
                     i+=1
-                total_sum+=curr_sum
-        if total_sum < 0:
-            return -1
-        return start_idx
+            total_sum+=curr_sum
+
+        return start_idx if total_sum >= 0 else -1
