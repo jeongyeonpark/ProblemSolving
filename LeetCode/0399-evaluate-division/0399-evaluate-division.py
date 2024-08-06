a40a34dcadb35a0501c4578fd903ce79750ad909
jeongyeonpark <120764374+jeongyeonpark@users.noreply.github.com>
2024-08-06 21:19:22 +0900

    Time: 36 ms (57.48%), Space: 16.7 MB (45.18%) - LeetHub

--- LeetCode/0399-evaluate-division/0399-evaluate-division.py
+++ LeetCode/0399-evaluate-division/0399-evaluate-division.py
@@ -1,71 +1,49 @@
+from typing import List
+
 class Solution:
+    def dfs(self, node: str, dest: str, gr: dict, vis: set, ans: List[float], temp: float) -> None:
+        if node in vis:
+            return
+
+        vis.add(node)
+        if node == dest:
+            ans[0] = temp
+            return
+
+        for ne, val in gr[node].items():
+            self.dfs(ne, dest, gr, vis, ans, temp * val)
+
+    def buildGraph(self, equations: List[List[str]], values: List[float]) -> dict:
+        gr = {}
+
+        for i in range(len(equations)):
+            dividend, divisor = equations[i]
+            value = values[i]
+
+            if dividend not in gr:
+                gr[dividend] = {}
+            if divisor not in gr:
+                gr[divisor] = {}
+
+            gr[dividend][divisor] = value
+            gr[divisor][dividend] = 1.0 / value
+
+        return gr
+
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        def clean_input(list1, list2):
-            for l in list1:
-                if l in list2:
-                    list1.remove(l)
-                    list2.remove(l)
-            return list1, list2
+        gr = self.buildGraph(equations, values)
+        finalAns = []
 
+        for query in queries:
+            dividend, divisor = query
 
-        result = [-1]*len(queries)
+            if dividend not in gr or divisor not in gr:
+                finalAns.append(-1.0)
+            else:
+                vis = set()
+                ans = [-1.0]
+                temp = 1.0
+                self.dfs(dividend, divisor, gr, vis, ans, temp)
+                finalAns.append(ans[0])
 
-        for i in range(len(equations)):
-            if equations[i] in queries:
-                idx = queries.index(equations[i])
-                result[idx] = values[i]
-
-        nums = {}
-        for i in range(len(equations)):
-            w1,w2 = clean_input(list(equations[i][0]), list(equations[i][1])) 
-            value = values[i] #w1/w2=value
-
-            if len(w1) == 1 and len(w2) == 0:
-                nums[w1[0]] = value
-                break
-            elif len(w1) == 0 and len(w2) == 1:
-                nums[w2[0]] = 1/value
-                break
-
-
-            if len(nums) == 0:
-                nums[w2[0]] = 1
-
-            for n in range(len(w1)):
-                if w1[n] in nums and w2[n] in nums:
-                    pass
-                elif not w1[n] in nums and w2[n] in nums:
-                    w1_v = nums[w2[n]]*value
-                    nums[w1[n]] = w1_v
-                elif w1[n] in nums and not w2[n] in nums:
-                    w2_v = nums[w1[n]]/value
-                    nums[w2[n]] = w2_v
-                else:
-                    nums[w2[n]] = 1
-                    w1_v = nums[w2[n]]*value
-                    nums[w1[n]] = w1_v
-
-        print(nums)
-        for j in range(len(queries)):
-            if result[j] == -1:
-                w1 = list(queries[j][0])
-                w2 = list(queries[j][1])
-
-                v1 = 1
-                v2 = 1
-                for w1_ in w1:
-                    if w1_ in nums:
-                        v1*=nums[w1_]
-                    else:
-                        v1 = 0
-
-                for w2_ in w2:
-                    if w2_ in nums:
-                        v2*=nums[w2_]
-                    else:
-                        v2 = 0
-
-                if v1 != 0 and v2 != 0:
-                    result[j] = v1/v2
-
-        return result
+        return finalAns
