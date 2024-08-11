59dad0077646d5525f02a9c888c72a3ec66a6f9f
jeongyeonpark <120764374+jeongyeonpark@users.noreply.github.com>
2024-08-11 14:42:40 +0900

    Time: 349 ms (69.18%), Space: 17.9 MB (34.59%) - LeetHub

--- LeetCode/0055-jump-game/0055-jump-game.py
+++ LeetCode/0055-jump-game/0055-jump-game.py
@@ -1,13 +1,38 @@
+# class Solution:
+#     def canJump(self, nums: List[int]) -> bool:
+#         result = [True]+[False]*(len(nums)-1)
+#         for i in range(len(nums)-1):
+#             if result[i] != False:
+#                 for j in range(1,nums[i]+1):
+#                     if i+j >= len(nums)-1:
+#                         return True
+#                     result[i+j] = True
+
+#         if result[-1] != False:
+#             return True
+#         return False
+
+
+# class Solution:
+#     def canJump(self, nums: List[int]) -> bool:
+#         dp = [False for _ in range(len(nums))]
+#         dp[-1] = True
+#         leftTrue = len(nums)-1 # 4
+
+#         for i in range(len(nums)-2, -1, -1): # [3, 2, 1, 0]
+#             maxPosition = min(len(nums)-1, i + nums[i]) # [4, 3, 4, 2]
+#             if maxPosition >= leftTrue:
+#                 dp[i] = True
+#                 leftTrue = i
+
+#         return dp[0]
+
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        result = [0]+[inf]*(len(nums)-1)
-        for i in range(len(nums)-1):
-            if result[i] != inf:
-                for j in range(1,nums[i]+1):
-                    if i+j >= len(nums)-1:
-                        return True
-                    result[i+j] = min(result[i]+1, result[i+j])
+        leftTrue = len(nums)-1 # 4
+        for i in range(len(nums)-2, -1, -1): # [3, 2, 1, 0]
+            maxPosition = i + nums[i] # [4, 3, 4, 2]
+            if maxPosition >= leftTrue:
+                leftTrue = i
 
-        if result[-1] != inf:
-            return True
-        return False
+        return True if leftTrue == 0 else False
