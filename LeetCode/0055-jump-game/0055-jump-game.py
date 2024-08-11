32cc1966e5d4f7cd896cf4680315510795c9a054
jeongyeonpark <120764374+jeongyeonpark@users.noreply.github.com>
2024-08-11 14:54:17 +0900

    Time: 353 ms (61.01%), Space: 17.8 MB (34.59%) - LeetHub

--- LeetCode/0055-jump-game/0055-jump-game.py
+++ LeetCode/0055-jump-game/0055-jump-game.py
@@ -8,24 +8,8 @@
 #                         return True
 #                     result[i+j] = True
 
-#         if result[-1] != False:
-#             return True
-#         return False
+#         return True if result[-1] != False else False
 
-
-# class Solution:
-#     def canJump(self, nums: List[int]) -> bool:
-#         dp = [False for _ in range(len(nums))]
-#         dp[-1] = True
-#         leftTrue = len(nums)-1 # 4
-
-#         for i in range(len(nums)-2, -1, -1): # [3, 2, 1, 0]
-#             maxPosition = min(len(nums)-1, i + nums[i]) # [4, 3, 4, 2]
-#             if maxPosition >= leftTrue:
-#                 dp[i] = True
-#                 leftTrue = i
-
-#         return dp[0]
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
