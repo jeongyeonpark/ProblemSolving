7ef533ec084bacef309d1610697b74514fb14097
jeongyeonpark <120764374+jeongyeonpark@users.noreply.github.com>
2024-09-05 22:01:40 +0900

    Time: 38 ms (57.85%), Space: 16.5 MB (94.27%) - LeetHub

--- LeetCode/0061-rotate-list/0061-rotate-list.py
+++ LeetCode/0061-rotate-list/0061-rotate-list.py
@@ -6,23 +6,26 @@ class ListNode:
 
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        if k == 0 or not head:
+        if not head:
             return head
 
-        nodes = []
+        len_, tail = 0, None
         curr_node = head
         while curr_node:
-            nodes.append(curr_node.val)
+            len_+=1
+            tail = curr_node
             curr_node = curr_node.next
 
-        if k%len(nodes)==0:
+        if k%len_ == 0:
             return head
+        k %= len_
 
-        n = k%len(nodes)
-        new_node = nodes[-n:]+nodes[:-n]
-        head2 = ListNode(new_node[0])
-        curr_node = head2
-        for i in new_node[1:]:
-            curr_node.next = ListNode(i)
-            curr_node = curr_node.next
-        return head2
+        curr = head
+        for _ in range(len_-k-1):
+            curr = curr.next
+
+        newhead = curr.next
+        curr.next = None
+        tail.next = head
+
+        return newhead
