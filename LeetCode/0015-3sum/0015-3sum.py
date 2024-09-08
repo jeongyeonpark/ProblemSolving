f19ee2f78da8ecdfa7c19faaa62ae9dde995213c
jeongyeonpark <120764374+jeongyeonpark@users.noreply.github.com>
2024-09-09 00:09:45 +0900

    Time: 515 ms (96.96%), Space: 21.1 MB (13.06%) - LeetHub

--- LeetCode/0015-3sum/0015-3sum.py
+++ LeetCode/0015-3sum/0015-3sum.py
@@ -1,75 +1,35 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = set()
+        r = set()
 
-        n, p, z = [], [], []
+        p, z, n = [], [], []
         for num in nums:
-            if num > 0:
+            if num>0:
                 p.append(num)
-            elif num < 0: 
+            elif num < 0:
                 n.append(num)
             else:
                 z.append(num)
 
-        N, P = set(n), set(p)
-
+        P, N = set(p), set(n)
         if z:
             for num in P:
                 if -1*num in N:
-                    res.add((-1*num, 0, num))
+                    r.add((-1*num, 0, num))
 
-        if len(z) >= 3:
-            res.add((0,0,0))
+        if len(z)>=3:
+            r.add((0,0,0))
 
         for i in range(len(n)):
             for j in range(i+1,len(n)):
-                target = -1*(n[i]+n[j])
-                if target in P:
-                    res.add(tuple(sorted([n[i],n[j],target])))
+                x = -1*(n[i]+n[j])
+                if x in P: #find set
+                    r.add(tuple(sorted([x, n[i], n[j]])))
 
         for i in range(len(p)):
             for j in range(i+1,len(p)):
-                target = -1*(p[i]+p[j])
-                if target in N:
-                    res.add(tuple(sorted([p[i],p[j],target])))
+                x = -1*(p[i]+p[j]) 
+                if x in N:#find set
+                    r.add(tuple(sorted([x, p[i], p[j]])))
 
-        return res
-
-# class Solution:
-#     def threeSum(self, nums: List[int]) -> List[List[int]]:
-#         res = set()
-
-#         pos = []
-#         zero = []
-#         neg = []
-#         for num in nums:
-#             if num>0:
-#                 pos.append(num)
-#             elif num < 0:
-#                 neg.append(num)
-#             else:
-#                 zero.append(num)
-
-#         POS = set(pos)
-#         NEG = set(neg)
-#         if zero:
-#             for P in POS:
-#                 if -1*P in NEG:
-#                     res.add((-1*P, 0, P))
-
-#         if len(zero)>2:
-#             res.add((0,0,0))
-
-#         for i in range(len(pos)):
-#             for j in range(i+1,len(pos)):
-#                 x = -1*(pos[i]+pos[j]) 
-#                 if x in neg:
-#                     res.add(tuple(sorted([x, pos[i], pos[j]])))
-
-#         for i in range(len(neg)):
-#             for j in range(i+1,len(neg)):
-#                 x = -1*(neg[i]+neg[j])
-#                 if x in pos:
-#                     res.add(tuple(sorted([x, neg[i], neg[j]])))
-
-#         return res
+        return r
