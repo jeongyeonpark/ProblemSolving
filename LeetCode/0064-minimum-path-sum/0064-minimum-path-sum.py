cd1fdcc8d5bb793d9bf98c1dbea083c00b373019
jeongyeonpark <120764374+jeongyeonpark@users.noreply.github.com>
2024-10-07 22:39:27 +0900

    Time: 125 ms (37.29%), Space: 18.5 MB (32.08%) - LeetHub

--- LeetCode/0064-minimum-path-sum/0064-minimum-path-sum.py
+++ LeetCode/0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,30 +1,24 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        start_x = 0
-        start_y = 0
         end_x = len(grid[0])
         end_y = len(grid)
 
-        q = deque([(0,0)])
-        visited = set()
-        while q:
-            for _ in range(len(q)):
-                x,y = q.popleft()
-                if not (x,y) in visited:
-                    visited.add((x,y))
-                    if x-1<0 and y-1<0:
-                        pass
-                    elif x-1<0 and y-1>=0:
-                        grid[y][x] = grid[y][x]+grid[y-1][x]
-                    elif x-1>=0 and y-1<0:
-                        grid[y][x] = grid[y][x]+grid[y][x-1]
-                    else: #x-1>= and y-1>=0
-                        grid[y][x] = min(grid[y][x]+grid[y-1][x], grid[y][x]+grid[y][x-1])
+        dp = [[0]*end_x for _ in range(end_y)]
+        dp[0][0] = grid[0][0]
 
-                    if x+1<end_x:
-                        q.append((x+1,y))
-                    if y+1<end_y:
-                        q.append((x,y+1))
+        v = dp[0][0]
+        for i in range(1,end_x):
+            dp[0][i] = v + grid[0][i]
+            v = dp[0][i]
 
-        return grid[-1][-1]
+        v = dp[0][0]
+        for j in range(1,end_y):
+            dp[j][0] = v + grid[j][0]
+            v = dp[j][0]
+
+        for x in range(1,end_x):
+            for y in range(1,end_y):
+                dp[y][x] = min(dp[y-1][x], dp[y][x-1]) + grid[y][x]
+
+        return dp[-1][-1]
 
